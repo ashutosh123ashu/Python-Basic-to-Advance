ac6bc6c9cb90fba54893c9d5607e40cd9c6e5da9
Ashutosh <37691012+ashutosh123ashu@users.noreply.github.com>
2021-07-10 09:02:36 +0530

    Update 1.Helloworld.py

--- 1.Helloworld.py
+++ 1.Helloworld.py
@@ -1,2 +1,17 @@
+# Python can be downloaded from "python.org" website. And the installation process is simple like installing a game.
+
+
+# Que 1. Why python?
+# Ans.  Python is a very easy to learn and flexible language. Python has a large community base so its easy to find solutions to your problem.
+#       It is recomended for machine learning purpose, it has various built in modules which helps in making machine learning models.
+
+# Que 2. What is a module?
+# Ans.  A module is a program written by someone and can be used by simply installing it in your program.
+#       The module can be added by simply importing it in your program. The example for import is give below after the questions.
+
+# Que 3. What is pip?
+# Ans.  pip is installation manager for python.
+
+
 import os
-print(os.listdir())
+print(os.listdir()) #listdir()--function will list all the files present in the folder.
