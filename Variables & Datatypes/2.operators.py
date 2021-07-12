b7239d9e2c860107d0ec69290083d642040c61fb
Ashutosh <37691012+ashutosh123ashu@users.noreply.github.com>
2021-07-12 12:35:43 +0530

    Add files via upload

---
+++ Variables & Datatypes/2.operators.py
@@ -0,0 +1,14 @@
+# print("The value of 3 + 4 is ",3+4)
+# # Assignment operators =, +=, -=  etc.
+# # Comparision operators
+
+# b = (12==3)
+# print (b)
+
+#Type casting
+a="234"
+b = int (a)
+print(b+5)
+print (type(a))
+c = str(b)
+print(type(c))
---
+++ Variables & Datatypes/3.input_function.py
@@ -0,0 +1,9 @@
+# a = input("Enter Your Name : ")
+# print(a)
+# print(type(a))
+# Everything taken from input function is of string type
+# It should be converted to required datatype after taking the input
+
+b = int(input("Enter a number: "))
+print(b)
+print(type(b))
