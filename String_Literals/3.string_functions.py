4cb00d80098f47daed90346dfcccbc7f15113d16
Ashutosh <37691012+ashutosh123ashu@users.noreply.github.com>
2021-07-14 22:00:37 +0530

    Add files via upload

---
+++ String_Literals/3.string_functions.py
@@ -0,0 +1,17 @@
+story = "Once upon a time there was a youtuber named Harry who uploaded python course with notes for free"
+
+# String Functions
+
+# 1. len()  --> returns length
+# print(len(story))
+
+# print(story.endswith("free")) # Checks whether the string "story"  ends with free or not. if it ends with free it will return true.
+
+# print(story.count("on")) # Counts no of on in string. 
+
+# print(story.capitalize()) # capitalizes the first letter.
+
+# print(story.find("Harry")) # Return the index at which Harry is present.
+# print(story.find("for"))
+
+# print(story.replace("Harry","CodeWithHarry")) # replaces Harry with code with harry.
