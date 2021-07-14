8781b1d1dff00a0ceda2d9bb95b47bdef8b2144f
Ashutosh <37691012+ashutosh123ashu@users.noreply.github.com>
2021-07-14 22:01:53 +0530

    Add files via upload

---
+++ String_Literals/5.practice_set_string.py
@@ -0,0 +1,61 @@
+# Que 1
+
+# name = input("Enter your name : ")
+# print("Goodmorning, "+ name)
+
+
+# Que  2  - write a program for a letter format 
+
+# letter = '''
+# Dear <|NAME|>,
+
+# You are selected!
+
+# Date: <|DATE|>
+
+# Thanks for participating in our online selection process.
+
+# Thanks & Regards
+# Admission team
+# '''
+# name = input("Enter your naem :")
+# date = input("Enter date: ")
+
+# letter = letter.replace("<|NAME|>", name)
+# letter = letter.replace("<|DATE|>", date)
+# print("******************************************************************")
+# print(letter)
+# print("******************************************************************")
+
+# Que  3 -Write a program to detect double spaces
+
+# st = "This is a string  with double  spacces"
+
+# doublespaces = st.replace("  "," ")
+# print(doublespaces)
+
+# Que 4 - Write a program to format the following letter using escape sequence characters
+#  letter = "Dear Harry, This python course is nice. Thanks!"
+
+letter = "Dear Harry, This python course is nice. Thanks!"
+print(letter)
+
+formatted_letter = "Dear Harry,\n\tThis course is nice\nThanks!"
+print(formatted_letter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ String_Literals/escape_sequences.py
@@ -0,0 +1,4 @@
+story ="Harry is good. \nHe is very tall" 
+print(story)
+
+# escape sequences are \n  ,  \t  ,  \\  ,\'   etc.
