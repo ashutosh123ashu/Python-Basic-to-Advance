c3eb10816b4c7855b5947aac95a1adfd6427c3c0
Ashutosh <37691012+ashutosh123ashu@users.noreply.github.com>
2021-07-15 22:41:03 +0530

    Create 1.list.py

---
+++ List_Tuples/1.list.py
@@ -0,0 +1,17 @@
+# Create a list using square bracket
+
+a = [1,2,3,46,9]
+a[0] = 34
+# print(a)
+# print(a[3])
+
+# We can create a list  with items of different types
+
+b = [45, "Harry", 7.8, 9, "Ashu"]
+# print(b)
+
+# List slicing
+
+friends = ["Harry", "Tom", "Rohan", 389, "Ashutosh", "Vani"] # Different datatypes elements can be added to the list
+print(friends[0:4])
+print(friends[-4:])
