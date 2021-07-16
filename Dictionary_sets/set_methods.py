7c0514b131b60ff82494f912fdc3259b9ac5fe2a
Ashutosh <37691012+ashutosh123ashu@users.noreply.github.com>
2021-07-16 22:04:35 +0530

    Add files via upload

---
+++ Dictionary_sets/set_methods.py
@@ -0,0 +1,22 @@
+b = {2,1,3}
+c = {4,7,8,3}
+
+# b.add(4)
+# b.add(6)
+# b.add((4,5,9))
+# # print(b)
+
+# print(len(b)) # Prints the length of b
+
+# b.remove(6)  # removes 6 from the set b  and throws an error while trying to remove an element which is not present 
+# print(b)
+
+# Pop
+
+# print(b.pop()) # removes a random element 
+
+# print(b.clear()) # empties the set
+
+# print(b.union(c)) # gives the union of two sets
+
+print(b.intersection(c)) # gives the common element between two
