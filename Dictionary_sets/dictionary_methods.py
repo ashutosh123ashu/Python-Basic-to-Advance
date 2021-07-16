41ac25793a3435372393f2e81fefdb6dd1143ad9
Ashutosh <37691012+ashutosh123ashu@users.noreply.github.com>
2021-07-16 22:02:07 +0530

    Add files via upload

---
+++ Dictionary_sets/dictionary_methods.py
@@ -0,0 +1,23 @@
+
+myDict = {
+    "fast": "In a Quick Manner",
+    "harry": "A Coder",
+    "marks": [1, 2, 5],
+    "anotherDict": {'harry': 'Player'}
+}
+
+# print(myDict.keys())  # It will print keys in dictionary
+
+# print(myDict.values())  # It will print values in dictionary
+
+# print(myDict.items())  # Prints the key,value pair of all items of the dictionary
+
+# print(myDict)
+# updateDict = {
+#     "lovish" : "Friends"
+# }
+# myDict.update(updateDict)  # will add the items at the end. Updates the value with key value pair
+# print(myDict)
+
+# print(myDict.get('harry2')) # If the key is not present in the dictionary then it will return none
+# print(myDict['harry2']) # If key is not present it will give error
