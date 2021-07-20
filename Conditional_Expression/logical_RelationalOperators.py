1c5ff02e30c5088a43073ed0d78582c6511388bf
Ashutosh <37691012+ashutosh123ashu@users.noreply.github.com>
2021-07-20 21:32:21 +0530

    Add files via upload

---
+++ Conditional_Expression/logical_RelationalOperators.py
@@ -0,0 +1,10 @@
+# Relational operators are AND OR and NOT
+
+
+
+age = int(input("Enter your age....\n"))
+
+if(age>34 and age<56):
+    print("Youn are eligible to work with us!")
+else:
+    print("You are not eligible to work with us!")
