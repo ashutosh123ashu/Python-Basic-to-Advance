0ac98b64636d712f6ab5efac7b2eca09c318d3e8
Ashutosh <37691012+ashutosh123ashu@users.noreply.github.com>
2021-07-20 21:30:11 +0530

    Create conditional.py

---
+++ Conditional_Expression/conditional.py
@@ -0,0 +1,28 @@
+# if-elif-else Ladder
+
+# Note: the identation can be given by a single tab or by giving 4 spaces
+
+
+# a = 45
+# if(a>3):
+#     print("The value is greater than 3")
+# elif(a>7):
+#     print("The value of a is grreater than 7")
+# elif(a>23):
+#     print("The value of a is greater than 23")
+# else:
+#     print("The value is greater than 23")
+
+
+#    ********************************************************************************
+# if-else statement
+# height=5.8
+
+if(height>5.5):
+    print("You are eligible for trial.")
+else:
+    print("Your height is less than 5.5, you are not eligible for army trial.")
+
+
+
+
