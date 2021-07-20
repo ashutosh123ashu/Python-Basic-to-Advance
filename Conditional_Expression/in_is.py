3cff94e204a966159ea1d3ff29ab776c27beefc3
Ashutosh <37691012+ashutosh123ashu@users.noreply.github.com>
2021-07-20 21:32:46 +0530

    Add files via upload

---
+++ Conditional_Expression/in_is.py
@@ -0,0 +1,13 @@
+
+# is   --> == It will check if the value of a is 45 or not.
+a = 45
+if(a is 45):
+     print("Yes")
+else:
+    print("No")
+
+
+# in --> Checks whether the element is present in some set or not. It will return True or False
+
+# a=[45, 56, 78]
+# print(56 in a ) # in uses for loop to iterate on a set
