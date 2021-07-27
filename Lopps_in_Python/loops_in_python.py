0654dd9c6cf7e9dd2485c137563bd7dab839eaaa
Ashutosh <37691012+ashutosh123ashu@users.noreply.github.com>
2021-07-27 21:47:19 +0530

    Create loops_in_python.py

---
+++ Lopps_in_Python/loops_in_python.py
@@ -0,0 +1,36 @@
+# There are mainly two types of loops in python
+# 1. for loop  2. while loop
+
+
+# print 1 - 50 using 50
+# i = 0
+# while(i<=50):
+#     print(i)
+#     i = i + 1  #  press ctrl+c to terminate infinite loop.
+
+# WAP to print all the items in the list using while loop.
+
+fruits = ["mango", "banana", "apple", "bhindi", "tamatar"]
+# i = 0
+# while(i<len(fruits)):
+#     print(fruits[i])
+#     i +=1
+
+
+# Write above program using for loop
+
+# for item in fruits:
+#     print(item)
+
+# range function is used to generate a sequence a numbers.
+#
+# for i in range(8):
+#     print(i) 
+
+# NOw if we want the count to start fro 1 or any other number we will do the following.
+
+for i in range(1,8):
+    print(i)
+
+
+# Note: we can define step size also  (start, stop, step_size)
