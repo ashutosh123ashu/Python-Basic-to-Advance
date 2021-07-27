0e4e8efbd518e572c781f61b2f6f0b82045852f6
Ashutosh <37691012+ashutosh123ashu@users.noreply.github.com>
2021-07-27 21:51:37 +0530

    Add files via upload

---
+++ Lopps_in_Python/practice_loops.py
@@ -0,0 +1,40 @@
+# Que 1.
+
+# num = int(input("Enter a number:  "))
+# for i in range(1,11):
+    # print(num * i) # Other  way is using f string.
+    # print(f"{num} x {i} = {num*i}")  # f-string is a very powerful tool to understand it makes life easier :)
+
+# Que 2. Write a program to greet to those whose name starts with S in the given list.
+
+
+# l1 = ["Harry", "Sohan", "Sachin", "Rahul"]
+
+# for name in l1:
+#     if name.startswith("S"):
+#         print("Hello " + name)
+
+# Que 3. WAP to find a given number is prime or not.
+
+# 4
+
+
+# Que 4. WAP to find facctorial
+# num = int(input("Enter a number:  "))
+# fact = 1 
+# for i in range(1,num+1):
+#      fact = i*fact
+# print(fact)
+
+# Que 5. WAP to print traingular star pattern
+
+# for i in range(4):
+#     print("*" *(i+1))  # practice star printing problems 
+
+
+
+
+
+
+
+
