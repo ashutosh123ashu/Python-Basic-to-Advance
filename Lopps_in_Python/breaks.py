e26557a142443b3cab4c04eb8758492dafb13b0b
Ashutosh <37691012+ashutosh123ashu@users.noreply.github.com>
2021-07-27 21:51:10 +0530

    Add files via upload

---
+++ Lopps_in_Python/breaks.py
@@ -0,0 +1,32 @@
+# break is used to get out of the loop.
+
+for i in range(10):
+    print(i)
+    if i==5:
+        break
+
+#************************************** To understand difference run both program one by one
+
+   
+for i in range(10):
+    print(i)
+    if i==5:
+        break
+else:
+    print("This is out of the loop") 
+
+    # Note: else will run only after successful termination of the for loop  but break is an unsucessful termination of the loop.
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
