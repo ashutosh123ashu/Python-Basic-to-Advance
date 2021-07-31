318ffc45c3e99c84d811ee2de4cfc0565cb32c2d
Ashutosh <37691012+ashutosh123ashu@users.noreply.github.com>
2021-07-31 09:37:21 +0530

    Add files via upload

---
+++ Funcyions_Recursions/function_quiz.py
@@ -0,0 +1,17 @@
+# WAP to greet user.
+
+
+# def greet(name):
+#     print("Good morning "+ name)
+
+# name = input("Enter your name: ")
+# greet(name)
+
+# function to add two numbers
+
+# def add(num1, num2):
+#     print(f" num1 + num2 = {num1 + num2}")
+
+# add(78,22)
+
+
